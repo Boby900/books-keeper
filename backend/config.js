75e11238e19c47b59f76f6e798288d8b278ae076
Boby900 <bobytiwari310@gmail.com>
2023-09-19 19:54:33 +0530

    deleted config.js

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  'mongodb+srv://bobyt2265:TXarpOpSo0jG8NM6@cluster1.pu2hb0t.mongodb.net/?retryWrites=true&w=majority';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
