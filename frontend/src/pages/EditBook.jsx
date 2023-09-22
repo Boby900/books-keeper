e43b4c104aa6b7b9dcf173166c5882fd931b7fe0
Boby900 <bobytiwari310@gmail.com>
2023-09-22 12:12:43 +0530

    updated EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -54,9 +54,9 @@ const EditBook = () => {
   return (
     <div className='p-4'>
       <BackButton />
-      <h1 className='text-3xl my-4'>Edit Book</h1>
+      <h1 className='text-3xl my-4 text-center'>Edit Book</h1>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+      <div className='mt-16 flex flex-col border-2 border-sky-400 rounded-xl max-w-full w-[600px] p-4 mx-auto'>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
